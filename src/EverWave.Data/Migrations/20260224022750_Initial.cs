d23b3e6b73379aeedf637c1220aa4cf6cf2bd4a7
BrenoReisSantos <breno.battle@gmail.com>
2026-02-24 01:15:39 -0300

    arquivos que sobraram

---
+++ src/EverWave.Data/Migrations/20260224022750_Initial.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EverWave.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CategoriaCargo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    InactivatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoriaCargo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Unidades",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Unidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cargos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descricao = table.Column<string>(type: "text", nullable: false),
+                    CategoriaCargoId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cargos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cargos_CategoriaCargo_CategoriaCargoId",
+                        column: x => x.CategoriaCargoId,
+                        principalTable: "CategoriaCargo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Membros",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    UnidadeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Membros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Membros_Unidades_UnidadeId",
+                        column: x => x.UnidadeId,
+                        principalTable: "Unidades",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CargoMembro",
+                columns: table => new
+                {
+                    CargosId = table.Column<int>(type: "integer", nullable: false),
+                    MembrosId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CargoMembro", x => new { x.CargosId, x.MembrosId });
+                    table.ForeignKey(
+                        name: "FK_CargoMembro_Cargos_CargosId",
+                        column: x => x.CargosId,
+                        principalTable: "Cargos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CargoMembro_Membros_MembrosId",
+                        column: x => x.MembrosId,
+                        principalTable: "Membros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contatos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Ddd = table.Column<string>(type: "text", nullable: false),
+                    Telefone = table.Column<string>(type: "text", nullable: false),
+                    Ddi = table.Column<string>(type: "text", nullable: false),
+                    MembroId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contatos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contatos_Membros_MembroId",
+                        column: x => x.MembroId,
+                        principalTable: "Membros",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ministerios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    MembroId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ministerios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ministerios_Membros_MembroId",
+                        column: x => x.MembroId,
+                        principalTable: "Membros",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MinisteriosLocais",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MinisterioId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UnidadeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    InactivatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MinisteriosLocais", x => x.Id);
+                    table.UniqueConstraint("AK_MinisteriosLocais_UnidadeId_MinisterioId", x => new { x.UnidadeId, x.MinisterioId });
+                    table.ForeignKey(
+                        name: "FK_MinisteriosLocais_Ministerios_MinisterioId",
+                        column: x => x.MinisterioId,
+                        principalTable: "Ministerios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MinisteriosLocais_Unidades_UnidadeId",
+                        column: x => x.UnidadeId,
+                        principalTable: "Unidades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParticipacoesMinisteriosLocais",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MinisterioLocalId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MembroId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CargoId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CargoId1 = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    InactivatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParticipacoesMinisteriosLocais", x => x.Id);
+                    table.UniqueConstraint("AK_ParticipacoesMinisteriosLocais_MinisterioLocalId_MembroId_C~", x => new { x.MinisterioLocalId, x.MembroId, x.CargoId });
+                    table.ForeignKey(
+                        name: "FK_ParticipacoesMinisteriosLocais_Cargos_CargoId1",
+                        column: x => x.CargoId1,
+                        principalTable: "Cargos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ParticipacoesMinisteriosLocais_Membros_MembroId",
+                        column: x => x.MembroId,
+                        principalTable: "Membros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ParticipacoesMinisteriosLocais_MinisteriosLocais_Ministerio~",
+                        column: x => x.MinisterioLocalId,
+                        principalTable: "MinisteriosLocais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CargoMembro_MembrosId",
+                table: "CargoMembro",
+                column: "MembrosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cargos_CategoriaCargoId",
+                table: "Cargos",
+                column: "CategoriaCargoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contatos_MembroId",
+                table: "Contatos",
+                column: "MembroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Membros_UnidadeId",
+                table: "Membros",
+                column: "UnidadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ministerios_MembroId",
+                table: "Ministerios",
+                column: "MembroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MinisteriosLocais_MinisterioId",
+                table: "MinisteriosLocais",
+                column: "MinisterioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipacoesMinisteriosLocais_CargoId1",
+                table: "ParticipacoesMinisteriosLocais",
+                column: "CargoId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParticipacoesMinisteriosLocais_MembroId",
+                table: "ParticipacoesMinisteriosLocais",
+                column: "MembroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CargoMembro");
+
+            migrationBuilder.DropTable(
+                name: "Contatos");
+
+            migrationBuilder.DropTable(
+                name: "ParticipacoesMinisteriosLocais");
+
+            migrationBuilder.DropTable(
+                name: "Cargos");
+
+            migrationBuilder.DropTable(
+                name: "MinisteriosLocais");
+
+            migrationBuilder.DropTable(
+                name: "CategoriaCargo");
+
+            migrationBuilder.DropTable(
+                name: "Ministerios");
+
+            migrationBuilder.DropTable(
+                name: "Membros");
+
+            migrationBuilder.DropTable(
+                name: "Unidades");
+        }
+    }
+}
