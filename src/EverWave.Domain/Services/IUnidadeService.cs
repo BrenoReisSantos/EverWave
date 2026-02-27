8082b0f30586888500303455d0e5311b3668e75a
BrenoReisSantos <breno.battle@gmail.com>
2026-02-26 23:00:44 -0300

    feat: adição de métodos na interface

--- src/EverWave.Domain/Services/IUnidadeService.cs
+++ src/EverWave.Domain/Services/IUnidadeService.cs
@@ -6,4 +6,7 @@ namespace EverWave.Domain.Services;
 public interface IUnidadeService
 {
     Task<Unidade?> CriaAsync(UnidadeCriacaoDto unidadeDto, CancellationToken cancellationToken);
+    Task<IEnumerable<Unidade>> ObtemTodosAsync(CancellationToken cancellationToken);
+    Task<Unidade?> ObtemAsync(Guid id, CancellationToken cancellationToken);
+    Task<Unidade> AtualizaAsync(Unidade unidade, CancellationToken cancellationToken);
 }
